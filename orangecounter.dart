2c8853670bbef962dd26417425a152678a3534f5
Abdulhadi Sultan Qureshi <hadiholoa@gmail.com>
2019-04-09 12:03:02 +0500

    git add -A

--- orangecounter.dart
+++ orangecounter.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 1;
 
   void _incrementCounter() {
     setState(() {
