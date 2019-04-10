c3a8383e95b256f281bf11d98a8e8012affdc7b0
Abdulhadi Sultan Qureshi <hadiholoa@gmail.com>
2019-04-11 00:53:16 +0500

    added the login as guest and sign up options, along with snackbars

--- lib/main.dart
+++ lib/main.dart
@@ -97,14 +97,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextStyle style = TextStyle(fontFamily: 'Roboto', fontSize: 19.0, color: Colors.black);
-  TextStyle butt = TextStyle(fontFamily: 'Roboto', fontSize: 25.0, color: Colors.white);
-
+  TextStyle butt = TextStyle(fontFamily: 'Roboto', fontSize: 23.0, color: Colors.white);
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  String emailstr = "";
+  String passstr = "";
+  // var snacktxt = "Email entered: " + emailstr + ", Pass entered: " + passstr; 
   @override
   Widget build(BuildContext context) {
 
     final emailField = TextField(
       obscureText: false,
       style: style,
+      onChanged: (text) {
+        emailstr = text;
+      },
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
           hintText: "Email",
@@ -114,11 +120,32 @@ class _MyHomePageState extends State<MyHomePage> {
     final passwordField = TextField(
       obscureText: true,
       style: style,
+      onChanged: (text) {
+        passstr = text;
+      },
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
           hintText: "Password",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+          
+    );
+    final signUpRdr = InkWell(
+      child: Text('Not a member? Sign up!'),
+      onTap: () {
+        final snackBar = SnackBar(
+            content: Text("Redirect to the sign up page."),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          _scaffoldKey.currentState.showSnackBar(snackBar);
+      }
     );
     final loginButon = Material(
       elevation: 5.0,
@@ -127,7 +154,20 @@ class _MyHomePageState extends State<MyHomePage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
-        onPressed: () {},
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text("Email entered: " + emailstr + ", Pass entered: " + passstr),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          _scaffoldKey.currentState.showSnackBar(snackBar);
+        },
         child: Text("Login",
             textAlign: TextAlign.center,
             style: butt.copyWith(
@@ -135,7 +175,36 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
 
+    final guestButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Color(0xff009aba),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text("Logging in as a guest."),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          _scaffoldKey.currentState.showSnackBar(snackBar);
+        },
+        child: Text("Login as a guest",
+            textAlign: TextAlign.center,
+            style: butt.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
     return Scaffold(
+      key: _scaffoldKey,
       resizeToAvoidBottomPadding: false,
       body: Center(
 
@@ -163,8 +232,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 loginButon,
                 SizedBox(
-                  height: 105.0,
+                  height: 15.0,
                 ),
+                guestButon,
+                SizedBox(
+                  height: 30.0,
+                ),
+                signUpRdr,
+                SizedBox(
+                  height: 40.0,
+                ),
+                
               ],
             ),
           ),
