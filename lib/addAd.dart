cf67d617aab3ab4f1ff65f80bf04559d432ecb4c
Ali Ahad <ali_thespy@hotmail.com>
2019-04-11 19:08:30 +0500

    Added more functionality including a screen for add a property and integration with firebase!

---
+++ lib/addAd.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+class AddAdState extends State<AddAd> {
+  TextStyle style = TextStyle(fontFamily: 'Roboto', fontSize: 19.0, color: Colors.black);
+  TextStyle butt = TextStyle(fontFamily: 'Roboto', fontSize: 23.0, color: Colors.white);
+  TextStyle signupstyle = TextStyle(fontFamily: 'Roboto', fontSize: 34.0, color: Color(0xff009aba));
+  final TextEditingController _desc = new TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  String description = "", namestr = "";
+  String passstr = "", cpassstr = "";
+  List<String> tags = [];
+  @override
+  Widget build(BuildContext context) {
+
+    final descField = TextField(
+      obscureText: false,
+      style: style,
+      textInputAction: TextInputAction.next,
+      controller: _desc,
+      onChanged: (text) {
+        description = text;
+      },
+      onEditingComplete: (){
+        setState(() {
+          tags.add(description);
+          description = "";
+          _desc.clear();
+          print(tags);
+          // FocusScope.of(context).requestFocus(_desc);
+        });
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Description",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final nameField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+        namestr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Name",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      onChanged: (text) {
+        passstr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+          
+    );
+    final cpasswordField = TextField(
+      obscureText: true,
+      style: style,
+      onChanged: (text) {
+        cpassstr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Confirm Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+          
+    );
+    final signUpRdr = InkWell(
+      child: Text('Not a member? Sign up!'),
+      onTap: () {
+        final snackBar = SnackBar(
+            content: Text("Redirect to the sign up page."),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change!
+              },
+            ),
+          );
+
+          // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+          _scaffoldKey.currentState.showSnackBar(snackBar);
+      }
+    );
+    final nextButon = Container(
+      padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width - 250.0, 0, 0, 0),    
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.orangeAccent[700],
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 12.0, 20.0, 12.0),
+          onPressed: () {
+            final snackBar = SnackBar(
+              content: Text("Name entered: " + namestr + ", Email entered: " + description + ", Pass entered: " + passstr + ", CPass entered: " + cpassstr),
+              action: SnackBarAction(
+                label: 'Undo',
+                onPressed: () {
+                  // Some code to undo the change!
+                },
+              ),
+            );
+
+            // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+            _scaffoldKey.currentState.showSnackBar(snackBar);
+           
+          },
+          child: Text("Post",
+              textAlign: TextAlign.center,
+              style: butt.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(title: new Text("Post Ad:"),),
+      body: Center(
+
+        child: ListView(
+          children: <Widget> [
+            Container(
+            color: Colors.white30,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // SizedBox(height: 50.0),
+                  // Text(
+                  //   "Sign Up",
+                  //   style: signupstyle,
+                  // ),
+                  SizedBox(height: 50.0),
+                  nameField,
+                  SizedBox(height: 10.0),
+                  descField,
+                  SizedBox(height: 10.0),
+                  passwordField,
+                  SizedBox(height: 10.0),
+                  cpasswordField,
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  nextButon,
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class AddAd extends StatefulWidget {
+  @override
+  AddAdState createState() => new AddAdState();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
+import 'listings.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
-			title: 'Flutter Demo',
+			title: 'Listings:',
 			theme: ThemeData(
 				primarySwatch: Colors.orange,
 			),
