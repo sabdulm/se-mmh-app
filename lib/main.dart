05bbd5e9915153b58132e9a2cf54403ed3cd0f57
Sheikh Abdul Mannan <sheikhabdulmannan@outlook.com>
2019-04-14 16:00:36 +0500

    deleted extra files

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,12 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'login.dart';
-=======
 // import 'listings.dart';
 import 'maps.dart';
 // import 'property.dart';
 // import 'addAd.dart';
 
 void main() => runApp(MyApp());
->>>>>>> dd88d6c28701a34017d5853c5bd8d8c2ea68049e
 
-void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-<<<<<<< HEAD
-      // This widget is the root of your application.
-      @override
-      Widget build(BuildContext context) {
-        return MaterialApp(
-          title: 'Market My House',
-          theme: ThemeData(
-            primarySwatch: Colors.orange,
-          ),
-          home: LoginPage(title: 'Market My House Login'),
-        );
-      }
-    }
-=======
 	// This widget is the root of your application.
 	@override
 	Widget build(BuildContext context) {
@@ -39,4 +20,3 @@ class MyApp extends StatelessWidget {
 	}
 }
 
->>>>>>> dd88d6c28701a34017d5853c5bd8d8c2ea68049e
