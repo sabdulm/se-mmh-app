f44357f080e514b435ba90a46e9b69ac8449a47a
ha-0797 <49429860+ha-0797@users.noreply.github.com>
2019-04-17 01:09:34 +0500

    Add files via upload

---
+++ lib/Profile-Edit.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import './drawer.dart';
+
+void main() => runApp(Edit());
+
+class Edit extends StatefulWidget {
+  @override
+  EditProfile createState() => EditProfile();
+}
+
+class EditProfile extends State<Edit> {
+  String nameStr = "";
+  String emailStr = "";
+  String passwordStr = "";
+  String cpasswordStr = "";
+  String addressStr = "";
+  String titleStr = "";
+  String mobileStr = "";
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  TextStyle style = TextStyle(fontFamily: 'Roboto', fontSize: 19.0, color: Colors.black);
+  @override
+  Widget build(BuildContext context) {
+    final nameField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+         nameStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Name",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+        emailStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      onChanged: (text) {
+        passwordStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final cpasswordField = TextField(
+      obscureText: true,
+      style: style,
+      onChanged: (text) {
+        cpasswordStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Confirm Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final addressField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+        addressStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Address",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final titleField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+        titleStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Title",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+    final mobileField = TextField(
+      obscureText: false,
+      style: style,
+      onChanged: (text) {
+        mobileStr = text;
+      },
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
+          hintText: "Mobile",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+    );
+
+    return Scaffold(
+      drawer: DrawerOnly(),
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      key: _scaffoldKey,
+      // resizeToAvoidBottomPadding: false,
+      body: Center(
+
+        child: ListView(
+          children: <Widget> [
+            Container(
+              color: Colors.white30,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 20.0),
+                    nameField,
+                    SizedBox(height: 20.0),
+                    emailField,
+                    SizedBox(height: 20.0),
+                    passwordField,
+                    SizedBox(height: 20.0),
+                    cpasswordField,
+                    SizedBox(height: 20.0),
+                    addressField,
+                    SizedBox(height: 20.0),
+                    titleField,
+                    SizedBox(height: 20.0),
+                    mobileField,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RaisedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              textColor: Colors.white,
+                              color: Colors.orange,
+                              padding: const EdgeInsets.all(0.0),
+                              child: Container(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('Back'),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            RaisedButton(
+                              onPressed: () {
+                                final snackBar = SnackBar(
+                                  content: Text("Name: " + nameStr + ", Email entered: " + emailStr + ", Pass entered: " + passwordStr + ", Address: " + addressStr + ", Title: " + titleStr + ", Mobile: " + mobileStr),
+                                  action: SnackBarAction(
+                                    label: 'Undo',
+                                    onPressed: () {
+                                      // Some code to undo the change!
+                                    },
+                                  ),
+                                );
+
+                                // Find the Scaffold in the Widget tree and use it to show a SnackBar!
+                                _scaffoldKey.currentState.showSnackBar(snackBar);
+                              },
+                              textColor: Colors.white,
+                              color: Colors.orange,
+                              padding: const EdgeInsets.all(0.0),
+                              child: Container(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('Submit'),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
