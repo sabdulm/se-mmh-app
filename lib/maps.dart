20942ed47a59cd8f27149ce74fdde51e66b41c12
Ali Ahad <ali_thespy@hotmail.com>
2019-04-17 11:54:53 +0500

    before fetch

--- lib/maps.dart
+++ lib/maps.dart
@@ -35,7 +35,7 @@ class MapSampleState extends State<MapSample> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
