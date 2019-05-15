cce9af19fac74d202bd09135c3af5517702a59cd
Ali Ahad <ali_thespy@hotmail.com>
2019-05-16 04:15:34 +0500

    fixed ammar's version

--- lib/Profile-Other.dart
+++ lib/Profile-Other.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import './drawer.dart';
 import './property.dart';
 import 'chat.dart';
-
+import 'appointment.dart';
 var _docId = '';
 
 class Profile extends StatefulWidget {
