b629947a1c30284600f4722b8506c1813347df8a
Sheikh Abdul Mannan <sheikhabdulmannan@outlook.com>
2019-05-16 13:19:23 +0500

    done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,37 +7,68 @@
 
 // import 'dart:io';
 
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:market_my_house/main.dart';
-import 'package:market_my_house/listings.dart';
+import 'package:market_my_house/main.dart';
+import 'package:market_my_house/property.dart';
+import 'package:market_my_house/signup1.dart';
+// import 'package:market_my_house/listings.dart';
 
 // import 'package:second/main.dart';
 
 void main() {
+
+  
+  print('Testing Login');
   testWidgets('Testing Login', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyHomePage());
+    await tester.pumpWidget(MyApp());
     expect(find.text('Email'), findsOneWidget);
-    // await tester.tapAt(find.byType(Material).at(0));
-    // await tester.enterText(find.byType(TextField).at(0), 'mannanabdul.ams@gmail.com');
-    // await tester.enterText(find.byType(TextField).at(1), '123456');
+    await tester.enterText(find.byType(TextField).at(0), 'mannanabdul.ams@gmail.com');
+    await tester.enterText(find.byType(TextField).at(1), '123456');
+    await tester.tap(find.byType(Material).at(0));
     // sleep(const Duration(seconds:5));
-    expect(find.text('Listings'), findsNothing);
-    // await tester.tap(find.byType(InkWell));
-    // await tester.enterText(find.byType(TextField), 'hello');
+    expect(find.text(''), findsNothing);
 
-
-
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
-
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
-
-    // // Verify that our counter has incremented.
-    // expect(find.text('1'), findsOneWidget);
   });
+  print('Testing Signup');
+  testWidgets('Testing Signup', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(SignUpPage1());
+    expect(find.text('Email'), findsOneWidget);
+    await tester.enterText(find.byType(TextField).at(0), 'hadi');
+    await tester.enterText(find.byType(TextField).at(1), 'hadi@gmail.com');
+    await tester.tap(find.byType(Material).at(0));
+    // sleep(const Duration(seconds:5));
+    expect(find.text(''), findsNothing);
+
+  });
+
+  print('Testing Property Page');
+  testWidgets('Testing Property', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(PropertyPage("-LevxzWafhxb4EA5D8Sg", "Property", null));
+    expect(find.text('Property Page'), findsNothing);
+    await tester.tap(find.byType(FlatButton).at(0));
+    await tester.tap(find.byType(FlatButton).at(1));
+    // sleep(const Duration(seconds:5));
+    expect(find.text(''), findsNothing);
+
+  });
+  print('Testing Drawer');
+  testWidgets('Testing Drawer', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(PropertyPage("-LevxzWafhxb4EA5D8Sg", "Property", null));
+    expect(find.text('Home'), findsNothing);
+    expect(find.text('Bookmarks'), findsNothing);
+    await tester.enterText(find.byType(TextField).at(0), 'mannanabdul.ams@gmail.com');
+    await tester.enterText(find.byType(TextField).at(1), '123456');
+    await tester.tap(find.byType(Material).at(0));
+    // sleep(const Duration(seconds:5));
+    expect(find.text(''), findsNothing);
+
+  });
+
+  
+  
 }
